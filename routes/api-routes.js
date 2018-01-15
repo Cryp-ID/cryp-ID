46b744907345162f670d6087af629ca07cd48ec7
P. Doyle <patrick.doyle126@gmail.com>
2018-01-15 17:20:04 -0600

    adding post and delete route to MongoDB

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -24,6 +24,29 @@ module.exports = function(app) {
                 res.send(saved);
             }
         })
-    })
+    });
+
+
+        // Delete One from the DB
+    app.get("/delete/:id", function(req, res) {
+      // Remove a note using the objectID
+      db.cryptoAddresses.remove({
+        "_id": mongojs.ObjectID(req.params.id)
+      }, function(error, removed) {
+        // Log any errors from mongojs
+        if (error) {
+          console.log(error);
+          res.send(error);
+        }
+        // Otherwise, send the mongojs response to the browser
+        // This will fire off the success function of the ajax request
+        else {
+          console.log(removed);
+          res.send(removed);
+        }
+      });
+    });
+
 
 }
+// 
